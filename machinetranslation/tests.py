aa5bcd5c3da4c310ccc779fb80edfb3c9b1bd429
Andrey <andreypomortsev@gmail.com>
2023-02-21 11:32:42 +0500

    Added docstring to the functions and to the class

--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -1,17 +1,24 @@
-import translator
 import unittest
+import translator
+
 
 class TranslatorTest(unittest.TestCase):
-    def test_empty_input_frenchToEnglish(self):
+    """Test translator.py with empty str, and 'Hello'"""
+    
+    def test_empty_input_french_to_english(self):
+        """Test frenchToEnglish with an empty input"""
         self.assertEqual(translator.frenchToEnglish(''), '')
 
-    def test_bonjour_frenchToEnglish(self):
+    def test_bonjour_french_to_english(self):
+        """Test frenchToEnglish with 'Bonjour' as the input"""
         self.assertEqual(translator.frenchToEnglish('Bonjour'), 'Hello')
 
-    def test_empty_input_englishToFrench(self):
+    def test_empty_input_english_to_french(self):
+        """Test englishToFrench with an empty input"""
         self.assertEqual(translator.englishToFrench(''), '')
 
-    def test_hello_englishToFrench(self):
+    def test_hello_english_to_french(self):
+        """Test englishToFrench with 'Hello' as the input"""
         self.assertEqual(translator.englishToFrench('Hello'), 'Bonjour')
 
 if __name__ == '__main__':
