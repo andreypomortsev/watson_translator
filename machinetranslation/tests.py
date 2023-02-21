4403d53b28f0e0f99c9ded5c96b48ef3c9757346
Andrey <andreypomortsev@gmail.com>
2023-02-21 11:22:40 +0500

    Broke down 2 test in 4

--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -1,11 +1,18 @@
 import translator
 import unittest
 
-class French(unittest.TestCase):
-    def test_frenchToEnglish(self):
+class TranslatorTest(unittest.TestCase):
+    def test_empty_input_frenchToEnglish(self):
         self.assertEqual(translator.frenchToEnglish(''), '')
+
+    def test_bonjour_frenchToEnglish(self):
         self.assertEqual(translator.frenchToEnglish('Bonjour'), 'Hello')
 
-    def test_englishToFrench(self):
+    def test_empty_input_englishToFrench(self):
         self.assertEqual(translator.englishToFrench(''), '')
+
+    def test_hello_englishToFrench(self):
         self.assertEqual(translator.englishToFrench('Hello'), 'Bonjour')
+
+if __name__ == '__main__':
+    unittest.main()
