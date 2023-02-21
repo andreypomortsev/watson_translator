fe73fe3c391a02e10b764c33580c8b52f41cd83b
Andrey <andreypomortsev@gmail.com>
2023-02-21 11:13:28 +0500

    Logic error fix

--- machinetranslation/tests.py
+++ machinetranslation/tests.py
@@ -4,8 +4,8 @@ import unittest
 class French(unittest.TestCase):
     def test_frenchToEnglish(self):
         self.assertEqual(translator.frenchToEnglish(''), '')
-        self.assertEqual(translator.frenchToEnglish('Hello'), 'Bonjour')
+        self.assertEqual(translator.frenchToEnglish('Bonjour'), 'Hello')
 
     def test_englishToFrench(self):
         self.assertEqual(translator.englishToFrench(''), '')
-        self.assertEqual(translator.englishToFrench('Bonjour'), 'Hello')
+        self.assertEqual(translator.englishToFrench('Hello'), 'Bonjour')
